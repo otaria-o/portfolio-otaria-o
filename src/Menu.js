d059dc9dc4eb61171265e8566c0f91d5126aa569
otaria-o <roberta.garavaglia.it@gmail.com>
2023-03-11 18:48:51 +0100

    menu  in orizzontale

--- src/Menu.js
+++ src/Menu.js
@@ -6,14 +6,14 @@ function Menu() {
       <Link style={{ textDecoration: "none", color: "black" }} to="/apps">
         <h2>APPS</h2>
       </Link>
-      <Link style={{ textDecoration: "none", color: "black" }} to="/gifs">
-        <h2>GIFS</h2>
-      </Link>
       <Link
         style={{ textDecoration: "none", color: "black" }}
         to="/contributions"
       >
         <h2>CONTRIBUTIONS</h2>
+      </Link>
+      <Link style={{ textDecoration: "none", color: "black" }} to="/gifs">
+        <h2>GIFS</h2>
       </Link>
     </nav>
   );
