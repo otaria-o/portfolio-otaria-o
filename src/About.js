947699eb72b6ca94c34e52c70497e0c0936fd9ee
otaria-o <112610682+otaria-o@users.noreply.github.com>
2023-11-06 11:44:49 +0100

    Update About.js

--- src/About.js
+++ src/About.js
@@ -18,17 +18,7 @@ function About() {
           <br/>rientrerò presto in Italia, in una città che può vantare un parco grandissimo. 
           <br/>vorrei trovare intanto il mio gene dell'introversione e coccolarlo tutta la notte. 
           <br/>tra poche settimane uscirà un mio post su un albo illustrato a cui tengo.
-          
-            // I grew up in Italy, close to Milan, and wanted to be a dancer.
-            // {/* <br />I have a Master's degree in Sociology and have recently
-            // completed an intensive Web Developer Full Stack bootcamp. I have */}
-            // <br />I worked in different fields: I have been a secretary, a
-            // social worker, a cashier and a bookseller.
-            // <br />I live with my husband and my daughter in Berlin. I like
-            // baking bread and cookies. I also enjoy going for walks, along the
-            // Spree and in the Tiergarten, sitting in a café and looking around,
-            // listening to my thoughts or to people's conversations. <br />I am
-            // passionate about books, especially picture books, and also poetry.
+           
           </p>
           <aside className="fotine">
             {/* <p className="arrow">&larr;</p> */}
