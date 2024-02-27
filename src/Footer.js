65b100099492642a9e621f4845281d1175a6066d
otaria-o <112610682+otaria-o@users.noreply.github.com>
2024-02-27 10:42:51 +0100

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -3,17 +3,11 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer className="footer">
-      <Link style={{ textDecoration: "none", color: "black" }} to="/credits">
-        <h2 style={{ fontSize: "small" }} className="">
-          CREDITS
-        </h2>
-      </Link>
+      
       <Link style={{ textDecoration: "none" }} to="/">
         <h2>roberta garavaglia</h2>
       </Link>
-      <Link style={{ textDecoration: "none", color: "black" }} to="/about">
-        <h2 style={{ fontSize: "small" }}>ABOUT</h2>
-      </Link>
+      
     </footer>
   );
 }
